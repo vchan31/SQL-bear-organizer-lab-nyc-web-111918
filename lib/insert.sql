3f2974a589f7604c03af941d009757842be9d037
Vincent Chan <vchan313@gmail.com>
2018-11-29 15:23:36 -0500

    did lab

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Mr. Chocolate', 10, 'Male', 'brown','Angry', True);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Rowdy', 10, 'Male', 'brown','Angry', True);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Tabitha', 6, 'F', 'brown','Angry', True);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Sergeant Brown', 10, 'Male', 'brown','Angry', True);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Melissa', 13, 'F', 'brown','Angry', True);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Grinch', 10, 'Male', 'brown','Angry', True);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Wendy', 6, 'F', 'brown','Angry', True);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (null, 10, 'Male', 'brown','Angry', True);
